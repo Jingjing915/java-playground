b0586da70572460a01fbbc29923638b68cb76fed
jingjinghuang <crystalhpku@gmail.com>
2023-04-19 13:59:12 -0700

    Code along

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -8,4 +8,10 @@ public class Cat extends Animal {
     public void play() {
         System.out.println("Cat " + name + " jumps on table");
     }
+
+    @Override
+    public void play(Toy toy) {
+        System.out.println("Cat " + name + " is playing the toy " + toy);
+    }
+
 }
